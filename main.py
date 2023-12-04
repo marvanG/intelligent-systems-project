1e563fabc34cb53c5714d1bf9c78bd0100e02346
marvanG <marius.vangeli@hotmail.segit config --global user.name marvanG>
2023-12-04 16:00:54 +0100

    Changes to website lab

--- main.py
+++ main.py
@@ -16,4 +16,30 @@ users = furhat.get_users()
 print(f'\nUsers: {users}')
 furhat.attend(user = 'CLOSEST')
 
-furhat.say(text = 'Hello, I am Furhat, your virtual robot. How are you today?', blocking=True)
+
+
+# functions
+def color(color): # changes lightning color
+    if color == "blue": # speaking
+        furhat.set_led(red=0, green=0, blue=200)
+    elif color == "green": # listening
+        furhat.set_led(red=0, green=200, blue=0)
+    elif color == "yellow": # thinking
+        furhat.set_led(red=200, green=200, blue=0)
+    elif color == "red": # error
+        furhat.set_led(red=200, green=0, blue=0)
+
+# Start a conversation
+color("blue")
+furhat.say(text = 'Hello, I am Mack, your virtual bartender. How are you today?', blocking=True)
+while True:
+
+    color("green")
+    result = furhat.listen()
+    print(f'User said: {result.message}')
+    print(result)
+    
+    break
+
+    
+
